4f90a44efdbb222eef7a6827033a12cb9cd6f079
OnoprienkoArtem <o.artem.e@gmail.com>
2018-06-21 22:02:17 +0300

    get all name users

--- callback/script.js
+++ callback/script.js
@@ -1,1 +1,24 @@
+//get all name users
+function loadUsers(callback) {
+    const request = new XMLHttpRequest();
+    request.open('GET', 'https://jsonplaceholder.typicode.com/users');
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState !== request.DONE) { return; }
+        const response = JSON.parse(request.responseText);
+        const div = document.querySelector('.demo');
+        const ul = document.createElement('ul');
+        div.appendChild(ul);
+        response.forEach(({ name }) => {
+            const li = document.createElement('li');
+            li.textContent = name;
+            ul.appendChild(li);
+        });
+        callback();
+    });
+    request.send();
+};
+
+loadUsers(() => { alert('all ok!!!');});
+
+
 
