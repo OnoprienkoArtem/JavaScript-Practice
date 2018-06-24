179ded89f01aeb790da5bd1a4dee657631ede099
OnoprienkoArtem <o.artem.e@gmail.com>
2018-06-24 23:35:50 +0300

    practice fetch add script

--- fetch/script.js
+++ fetch/script.js
@@ -1,1 +1,22 @@
+var status = function(response) {
+    if(response.status !== 200) {
+        return Promise.reject(new Error(response.statusText));
+    }
+    return Promise.resolve(response);
+}
 
+var json = function(response) {
+    return response.json();
+}
+
+fetch('https://jsonplaceholder.typicode.com/users', {
+    method: 'post'
+})
+    .then(status)
+    .then(json)
+    .then(function(data) {
+        console.log('data', data)
+    })
+    .catch(function(error) {
+        console.log('error', error)
+    })
