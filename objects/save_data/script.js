5f0a721ea9c948794fb55ebfdfb0418303f05b40
OnoprienkoArtem <o.artem.e@gmail.com>
2018-07-04 23:14:22 +0300

    practice work with object

---
+++ objects/save_data/script.js
@@ -0,0 +1,47 @@
+var obj = {};
+var retObj_0 = {};
+var retObj = {};
+
+function checks() {
+    var cbx = document.getElementsByTagName("input"),
+        mas = [];
+    for (i = 0; i < cbx.length; i++) {
+        if (cbx[i].type == "checkbox" && cbx[i].checked) {
+            mas.push(cbx[i].value);
+            retObj_0['q0'] = mas;
+        }
+    }
+    var sObj = JSON.stringify(retObj_0);
+    localStorage.setItem("object_0", sObj);
+    console.log('1.', localStorage.object_0);
+    retObj_0 = JSON.parse(localStorage.getItem("object_0"));
+}
+
+var q1 = document.getElementsByClassName('q_1');
+for (var i = 0; i < q1.length; i++) {
+    q1[i].addEventListener('click', function (e) {
+        obj['q1'] = e.target.innerText;
+        console.log(obj);
+    })
+}
+var q2 = document.getElementsByClassName('q_2');
+for (var i = 0; i < q2.length; i++) {
+    q2[i].addEventListener('click', function (e) {
+        obj['q2'] = e.target.innerText;
+        console.log(obj);
+    })
+}
+
+var buttons = document.getElementsByName("note");
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", getValue);
+}
+function getValue(event) {
+    obj['vote'] = event.target.value;
+    console.log(obj);
+}
+
+
+
+
+
