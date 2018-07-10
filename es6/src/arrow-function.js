33c861773857e1283879d01b499efc372610536c
OnoprienkoArtem <o.artem.e@gmail.com>
2018-07-10 22:23:59 +0300

    practice arrow function

--- es6/src/arrow-function.js
+++ es6/src/arrow-function.js
@@ -70,3 +70,17 @@ console.log(getPerson_arrow());
 
 (() => console.log('IIFE'))();
 
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+let sum = 0;
+
+// numbers.forEach(function(num) {
+//     sum += num;
+// });
+// console.log(sum);
+
+numbers.forEach(num => sum += num);
+console.log(sum);
+
+let squared = numbers.map(n => n * n);
+console.log(squared);
