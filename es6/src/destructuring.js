d07d5d92208329bd90e83b2be3969d5349ae6107
OnoprienkoArtem <o.artem.e@gmail.com>
2018-07-14 23:02:52 +0300

    practice destructuring

---
+++ es6/src/destructuring.js
@@ -0,0 +1,30 @@
+let languages = ['JS', 'PHP', 'Python', 'Ruby'];
+
+// let js = languages[0];
+// let php = languages[1];
+// let py = languages[2];
+// let rb = languages[3];
+
+// let [js, php, py, rb] = languages;
+
+let [js, php, py, rb] = ['JS', 'PHP', 'Python', 'Ruby'];
+
+console.log(js, php, py, rb);
+
+
+// let score = [3, 4, 5];
+// let [low, mid, high] = score;
+// console.log(low, mid, high);
+
+
+// let score = [3, 4, 5, 6, 7];
+// let [low, ...rest] = score;
+// console.log(low, rest);
+
+
+// let score_1 = [3, 5, [6, 7]];
+// let [low, mid, [high, higher]] = score_1;
+// console.log(low, mid, high, higher);
+
+
+
