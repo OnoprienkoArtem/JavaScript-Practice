1992e1e5e433ed299d00d5b5b1f0295eb96d26c8
OnoprienkoArtem <o.artem.e@gmail.com>
2018-07-15 22:27:44 +0300

    practice destructing objects es6

--- es6/src/destructuring_object.js
+++ es6/src/destructuring_object.js
@@ -1,16 +1,29 @@
-ES5
-var person = {
+// ES5
+// var person = {
+//     firstName: 'John',
+//     lastName: 'Doe'
+// }
+//
+// var firstName = person.firstName;
+// var lastName = person.lastName;
+// console.log(firstName, lastName);
+
+
+
+
+ES6
+let person = {
     firstName: 'John',
     lastName: 'Doe'
 }
 
-var firstName = person.firstName;
-var lastName = person.lastName;
+let { firstName: fn, lastName: ln } = { firstName: 'John', lastName: 'Doe' };
+
+let { firstName, lastName } = person;
 console.log(firstName, lastName);
 
-
-
-
+let { firstName: fn, lastName: ln } = person;
+console.log(fn, ln);
 
 
 
