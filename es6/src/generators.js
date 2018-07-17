a9b877d7470ac96fbd7948079e79669371fd9e1e
OnoprienkoArtem <o.artem.e@gmail.com>
2018-07-17 22:14:27 +0300

    practice generators example

--- es6/src/generators.js
+++ es6/src/generators.js
@@ -1,22 +1,55 @@
-function* range(start, end) {
-    let current = start;
-    while (current <= end) {
-        yield current++;
+// function* range(start, end) {
+//     let current = start;
+//     while (current <= end) {
+//         yield current++;
+//     }
+// }
+
+// for (let num of range(1, 10)) {
+//     console.log(num);
+// }
+
+// function* generate() {
+//         console.log('start');
+//         yield 1;
+//         yield 2;
+//         yield 3;
+//         console.log('finish');
+//     }
+
+function generate() {
+    let currunt = 1;
+    console.log('Start');
+    return {
+        [Symbol.iterator]() {
+            return {
+                next() {
+                    let result = { value: undefined, done: true };
+
+                    if (currunt <= 3) {
+                        result.value = currunt;
+                        result.done = false;
+                        currunt++;
+                    } else {
+                        result.value = undefined;
+                        result.done = true;
+                        console.log('Finish');
+                    }
+                    return result;
+                }
+            }
+        }
     }
 }
 
-for (let num of range(1, 10)) {
-    console.log(num);
-}
 
-function* generate() {
-        console.log('start');
-        yield 1;
-        yield 2;
-        yield 3;
-        console.log('finish');
-    }
+console.log(typeof generate);
 
+let iterator = generate()[Symbol.iterator]();
 
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
 
 
