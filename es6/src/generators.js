a94fc4b12c5910544b584d725573895dae635bc6
OnoprienkoArtem <o.artem.e@gmail.com>
2018-07-17 22:14:59 +0300

    practice generators example

--- es6/src/generators.js
+++ es6/src/generators.js
@@ -17,39 +17,51 @@
 //         console.log('finish');
 //     }
 
-function generate() {
-    let currunt = 1;
-    console.log('Start');
-    return {
-        [Symbol.iterator]() {
-            return {
-                next() {
-                    let result = { value: undefined, done: true };
+// function generate() {
+//     let currunt = 1;
+//     console.log('Start');
+//     return {
+//         [Symbol.iterator]() {
+//             return {
+//                 next() {
+//                     let result = { value: undefined, done: true };
 
-                    if (currunt <= 3) {
-                        result.value = currunt;
-                        result.done = false;
-                        currunt++;
-                    } else {
-                        result.value = undefined;
-                        result.done = true;
-                        console.log('Finish');
-                    }
-                    return result;
-                }
-            }
+//                     if (currunt <= 3) {
+//                         result.value = currunt;
+//                         result.done = false;
+//                         currunt++;
+//                     } else {
+//                         result.value = undefined;
+//                         result.done = true;
+//                         console.log('Finish');
+//                     }
+//                     return result;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+// console.log(typeof generate);
+
+// let iterator = generate()[Symbol.iterator]();
+
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+
+
+let numbers = {
+    * range(start, end) {
+        let current = start;
+        while (current <= end) {
+            yield current++;
         }
     }
+};
+
+for (let num of numbers.range(1, 10)) {
+    console.log(num);
 }
-
-
-console.log(typeof generate);
-
-let iterator = generate()[Symbol.iterator]();
-
-console.log(iterator.next());
-console.log(iterator.next());
-console.log(iterator.next());
-console.log(iterator.next());
-
-
