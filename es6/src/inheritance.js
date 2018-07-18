f4df2c455260589095838213106e5297db9f8f4e
OnoprienkoArtem <o.artem.e@gmail.com>
2018-07-18 22:57:48 +0300

    practice inheritance create class

---
+++ es6/src/inheritance.js
@@ -0,0 +1,19 @@
+class Task {
+    constructor(title) {
+        this._title = title;
+        this.done = false;
+        Task.count += 1;
+        console.log('Создание задачи');
+
+    };
+
+
+}
+
+
+
+
+
+
+
+
