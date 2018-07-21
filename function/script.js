9140e68a3936a7209d234645507da8013e17c363
OnoprienkoArtem <o.artem.e@gmail.com>
2018-07-21 22:20:28 +0300

    repeat function

--- function/script.js
+++ function/script.js
@@ -8,3 +8,34 @@ function runFunction(param) {
 };
 
 runFunction(firstFunction);
+
+
+
+
+var seconfFunction = function() {
+    return 'Run secondFunction.';
+}
+
+function runSecondFunction(param) {
+    var first = 'Run runSecondFunction';
+    var last = param(); 
+    return first + last;
+}
+
+console.log(runSecondFunction(seconfFunction));
+
+
+
+
+
+setTimeout(function() {
+    console.log('Anonymec Function.');
+}, 2000);
+
+
+
+var setTO = function() {
+    console.log('Anonymec Function.');
+}
+
+setTimeout(setTO, 2000)
