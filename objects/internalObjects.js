4b153edf48188f3e462dbbd1120392db42263ef1
OnoprienkoArtem <o.artem.e@gmail.com>
2018-07-25 22:45:41 +0300

    repeat internal object

---
+++ objects/internalObjects.js
@@ -0,0 +1,10 @@
+// STRING
+
+const user = 'John';
+const system = new String('windows');
+
+//  \n ne line
+
+console.log(system.length);
+console.log(system.toLocaleLowerCase());
+console.log(system.toLocaleUpperCase());
