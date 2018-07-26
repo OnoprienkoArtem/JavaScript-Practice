816d0b3f22e390d7ee0c3673489838f8fc6a7e5b
OnoprienkoArtem <o.artem.e@gmail.com>
2018-07-26 22:09:34 +0300

    practice internal objects

--- objects/internalObjects.js
+++ objects/internalObjects.js
@@ -3,8 +3,32 @@
 const user = 'John';
 const system = new String('windows');
 
-//  \n ne line
+//  \n new line
 
 console.log(system.length);
 console.log(system.toLocaleLowerCase());
 console.log(system.toLocaleUpperCase());
+
+
+
+
+const text = 'Life is good, life is great';
+const key = 'life';
+
+const firstPos = text.indexOf(key);
+console.log(firstPos); //14 - index match
+
+const secondPos = text.indexOf('bad');
+console.log(secondPos); // -1 - not found
+
+const positon = text.lastIndexOf('is');
+console.log(positon); // 22
+
+
+
+const language = ' JavaScriot ';
+console.log(language);
+const trimLanguage = language.trim(); // Удаляет пробелы с начала и конца строки
+console.log(trimLanguage);
+
+
