3ff5982e700216c5267adb776d93f4f106d5358e
OnoprienkoArtem <o.artem.e@gmail.com>
2018-07-27 23:39:10 +0300

    repeat arrays

---
+++ array/arrays.js
@@ -0,0 +1,37 @@
+const array = [];
+const newArray = new Array();
+
+console.log(typeof array); // object
+
+const array1 = ['HTML', 'CSS'];
+console.log(array1);
+
+array1[2] = 'JS';
+console.log(array1);
+console.log(array1.length);
+
+
+let arrayG = [1, 2, 3, 4];
+let newArrayG = arrayG;
+console.log(arrayG);
+console.log(newArrayG);
+
+newArrayG[2] = 555;
+console.log(arrayG);
+console.log(newArrayG);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
