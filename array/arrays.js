145c0e22da66f7b378b6b6321c1f73202b39f6fb
OnoprienkoArtem <o.artem.e@gmail.com>
2018-07-27 23:40:09 +0300

    repeat arrays methods

--- array/arrays.js
+++ array/arrays.js
@@ -22,7 +22,34 @@ console.log(newArrayG);
 
 
 
+// ARRAY METHODS 
 
+// push() - добавляет в конец массива елементы и возвращает его.
+const array2 = ['html'];
+array2.push('css');
+console.log(array2);
+
+const newArray2 = ['HTML'];
+for (let i = 0; i <= 10; i++) {
+    newArray2.push('new index is ' + i);
+}
+console.log(newArray2);
+
+
+// pop() - удаляет последний элемент массиваб уменьшает длинну, и возвращает удаленное значение.
+const arrayPop = ['html', 'css', 'js'];
+console.log(arrayPop);
+
+const deleted = arrayPop.pop();
+console.log(arrayPop);
+console.log(deleted);
+
+
+// unshift() - добавляет элементы или элементы в начало массива.
+const arrayUnshift = ['html', 'css', 'js'];
+console.log(arrayUnshift); //["html", "css", "js"]
+arrayUnshift.unshift(true);
+console.log(arrayUnshift); //[true, "html", "css", "js"]
 
 
 
