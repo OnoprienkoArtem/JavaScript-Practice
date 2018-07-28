97d2f42e78e831baaca4af04b68de97188fe9cd5
OnoprienkoArtem <o.artem.e@gmail.com>
2018-07-28 22:37:10 +0300

    repeat array iterators

---
+++ array/iterators.js
@@ -0,0 +1,19 @@
+// перебор свойств
+
+// for
+// заполняем пустой массив
+const arrayOne = [];
+for (let i = 0; i <= 5; i++) {
+    arrayOne[i] = `value ${i}`;
+}
+
+console.log(arrayOne); // ["value 0", "value 1", "value 2", "value 3", "value 4", "value 5"]
+
+const arrayOnePush = [];
+for (let i = 0; i <= 5; i++) {
+    arrayOnePush.push(`value ${i}`);
+}
+
+console.log(arrayOnePush); // ["value 0", "value 1", "value 2", "value 3", "value 4", "value 5"]
+
+
