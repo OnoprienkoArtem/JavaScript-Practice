253fa11241f3db09c7df6162e8124da6ed997677
OnoprienkoArtem <o.artem.e@gmail.com>
2018-07-28 22:38:10 +0300

    repeat for of

--- array/iterators.js
+++ array/iterators.js
@@ -17,3 +17,27 @@ for (let i = 0; i <= 5; i++) {
 console.log(arrayOnePush); // ["value 0", "value 1", "value 2", "value 3", "value 4", "value 5"]
 
 
+// for ... of
+const arrayForOf = ['one', 'two', 'three', 'four', 'five'];
+for (let i of arrayForOf) {
+    console.log(i);
+}
+// one
+// two
+// three
+// four
+// five
+
+
+const arrayForOfCondition = ['one', 'two', 5, 'three', 'four', 'five', null, 9];
+for (let i of arrayForOfCondition) {
+    if (typeof i === 'string') {
+        continue;
+    }
+
+    if (i === null) {
+        break;        
+    }
+    console.log(i);
+}
+// 5
