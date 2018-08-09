3372e65295bdff6b06fe53343ccd48c8c9908252
OnoprienkoArtem <o.artem.e@gmail.com>
2018-08-09 23:11:08 +0300

    review some tricks 2

--- _secret-tricks/script.js
+++ _secret-tricks/script.js
@@ -13,3 +13,14 @@ console.log(csum(1)); // b => 1 + b
 
 const plusOne = csum(1);
 console.log(plusOne(2)); // 3
+
+
+// work with array
+// map();
+console.log([1, 2, 3].map(x => x + 1)); // [2, 3, 4]
+
+const arr1 = [1, 2, 3];
+const plusOne1 = x => x + 1;
+console.log(arr1.map(x => plusOne1(x))); // [2, 3, 4]
+//or =>  this two constructions are equivalent
+console.log(arr1.map(plusOne1)); // [2, 3, 4]
