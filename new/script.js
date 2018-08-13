d1eafad690ff09eb3f380c789c3ac6bfa3488245
OnoprienkoArtem <o.artem.e@gmail.com>
2018-08-13 22:29:38 +0300

    practice function constructor

--- new/script.js
+++ new/script.js
@@ -4,10 +4,32 @@
 
 function Foo() {}
 
-const Foo2 = new Foo();
+const Foo2 = new Foo(); // создаем новый объект
 
 console.log(typeof Foo2); // object
 
 
-// ФК - может содержать свойства, которые при инициализации экзмпляра объекта, будут принадлежать этому объекту.
-// Для этого используем this.
+// ФК - может содержать свойства, которые при инициализации экзмпляра объекта, будут принадлежать этому объекту,
+// для этого используем this.
+
+
+function Room(name, area) {
+    this.area = area;
+    this.name = name;
+
+    this.showInfo = function() {  // как публичный метод
+        console.log(`Type of room: ${this.name}, area: ${this.area}`);
+    }
+}
+
+const guestRoom = new Room('Guest', 12);
+console.log(guestRoom); // Room {area: 12, name: "Guest"}
+
+console.log(guestRoom.area); // 12
+console.log(guestRoom.name); // Guest
+
+const room1 = new Room('Bedroom', 15);
+const room2 = new Room('Bath', 10);
+
+room1.showInfo(); // Type of room: Bedroom, area: 15
+room2.showInfo(); // Type of room: Bath, area: 10
