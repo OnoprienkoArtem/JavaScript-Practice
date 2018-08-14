a61f0b124d1c0b4dea16bd1609ff786e31824316
OnoprienkoArtem <o.artem.e@gmail.com>
2018-08-14 23:50:25 +0300

    way to save needed context with self

--- this/lost-contest.js
+++ this/lost-contest.js
@@ -50,7 +50,43 @@ const room1 = new Room('Guest', 15);
 room1.getFullDescription(); // Type of room: undefined, area: undefined
 
 
+//--------------------------------------
+// СПОСОБЫ СОХРАНЕНИЯ НУЖНОГО КОНТЕКСТА
 
+
+
+// --- сохранение this в переменную;
+let user2_2 = {
+    firstName: 'John',
+    secondName: 'Hook',
+    getFullName: function () {        
+        const self = this;
+
+        setTimeout(function () {
+            console.log(`Full name: ${self.firstName} ${self.secondName}`);
+        }, 2000);
+    }
+}
+user2_2.getFullName(); //  Full name: John Hook
+
+
+
+function Room2_2(type, area) {
+    let self = this;
+    this.area = area;
+    this.type = type;
+
+    function showInfo() {
+        console.log(`Type of room: ${self.type}, area: ${self.area}`);
+    }
+
+    this.getFullDescription = function () {
+        showInfo();
+    }
+}
+
+const room1_2 = new Room2_2('Guest', 15);
+room1_2.getFullDescription(); // Type of room: Guest, area: 15
 
 
 
