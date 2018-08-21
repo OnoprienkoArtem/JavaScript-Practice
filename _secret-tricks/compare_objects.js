b50194fda915f2e6a08bf1e69061108089e29ca5
OnoprienkoArtem <o.artem.e@gmail.com>
2018-08-21 23:38:18 +0300

    function wiche compare objects

--- _secret-tricks/compare_objects.js
+++ _secret-tricks/compare_objects.js
@@ -15,3 +15,23 @@ let person3 = {
     age: '35', // match fields   
     car: 'Kia' // match fields
 }
+
+function comparePerson(obj1, obj2) {
+    let out = {}
+    let keys1 = Object.keys(obj1)
+    let keys2 = Object.keys(obj2)
+
+    for (let k1 of keys1) {
+        flag = false;
+        for (let k2 of keys2) {
+            if (k1 === k2) {
+                flag = true;
+                out[k1] = obj2[k2];
+            }
+        }
+        if (!flag) {
+            out[k1] = obj1[k1];
+        }
+    }
+    return out;
+}
