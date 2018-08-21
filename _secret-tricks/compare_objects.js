6bf9ddefc88dd6ff14b34f8189f13c248adff83f
OnoprienkoArtem <o.artem.e@gmail.com>
2018-08-21 23:33:04 +0300

    create three diferent objects

---
+++ _secret-tricks/compare_objects.js
@@ -0,0 +1,17 @@
+let person1 = {
+    name: 'Tim', // match fields
+    surname: 'Smith',
+    age: '25', // match fields
+    car: 'Toyota' // match fields
+}
+
+let person2 = {
+    name: 'John', // match fields
+    position: 'JS'
+}
+
+let person3 = {
+    name: 'Art', // match fields
+    age: '35', // match fields   
+    car: 'Kia' // match fields
+}
