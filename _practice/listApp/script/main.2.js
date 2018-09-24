8e348bd9b2bb0c91649fbe48fbdcefeffe9e8ecd
OnoprienkoArtem <o.artem.e@gmail.com>
2018-09-24 22:44:41 +0300

    new kind organaise code

---
+++ _practice/listApp/script/main.2.js
@@ -0,0 +1,19 @@
+var itemsListComponent = (function () {
+    const userList = document.querySelector("#user-list");
+    const selectAll = document.querySelector("#select-all");
+    const statsInfo = document.querySelector("#stats");
+
+    const emailDropdown = document.querySelector("#dropdown-email");
+    const roleDropdown = document.querySelector("#dropdown-role");
+    const search = document.querySelector("#inputSearch");
+    const nextBtn = document.querySelector("#next-page");
+    const backBtn = document.querySelector("#back-btn");
+
+    const detailsView = document.querySelector("#details-view");
+    const mainView = document.querySelector("#main-view");
+    const detailsItems = document.querySelector("#details-items");
+
+
+
+  
+}())
