3baf1270534c1c4fc97fd408bff02d5ecb3a3aec
OnoprienkoArtem <o.artem.e@gmail.com>
2018-10-15 23:20:15 +0300

    practice algortms

--- _practice/fibonacci/script.js
+++ _practice/fibonacci/script.js
@@ -7,9 +7,23 @@ function fibonacci(n) {
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-console.log(fibonacci(5));
-console.log(fibonacci(10));
+console.log(fibonacci(5)); 
+console.log(fibonacci(10)); // 55
 
 //console.log(fibonacci(10));  будет выполнятся 50000 лет
 
 
+
+function fibEffective(n) {
+    var arr = [];
+    arr[0] = 0;
+    arr[1] = 1;
+
+    for (let index = 2; index <= n; index++) {
+        arr[index] = arr[index - 1] + arr[index - 2];                
+    }
+    return arr[n];
+}
+
+console.log(fibEffective(10)); // 55
+console.log(fibEffective(100)); // 54224848179262000000
