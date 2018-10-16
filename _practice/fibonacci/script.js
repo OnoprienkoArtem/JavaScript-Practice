72f13bf229090aafb145a794e9e40cea86615374
OnoprienkoArtem <o.artem.e@gmail.com>
2018-10-16 23:08:53 +0300

    add method memoisation

--- _practice/fibonacci/script.js
+++ _practice/fibonacci/script.js
@@ -37,4 +37,14 @@ for (var j = 0; j < arrMemo.length; j++) {
     arrMemo[j] = -1;
 }
 
+function memo(n, arr) {
+    if (arr[n] != -1) return arr[n];    
+    if (n <= 1) return n;
 
+    var result = memo(n - 1, arr) + memo(n - 2, arr);
+    arr[n] = result;
+
+    return result;
+}
+
+console.log(memo(n, arrMemo)); // 354224848179262000000
