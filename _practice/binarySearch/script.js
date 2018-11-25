5f2a65b8d7385878867921702cfeaa72ec93cfd4
OnoprienkoArtem <o.artem.e@gmail.com>
2018-11-25 23:05:25 +0200

    binary search in array

---
+++ _practice/binarySearch/script.js
@@ -0,0 +1,22 @@
+var arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+function binarySearch(a, target) {
+    debugger
+    var low = 0;
+    var high = a.length - 1;
+
+    while (low <= high) {
+        var middle = low + (high - low) / 2;
+
+        if(target < a[middle]) {
+            high = middle - 1;
+        } else if(target > a[middle]) {
+            low = middle + 1;
+        } else {
+            return middle;
+        }
+    }
+    return -1;
+}
+
+console.log(binarySearch(arr, 3));
