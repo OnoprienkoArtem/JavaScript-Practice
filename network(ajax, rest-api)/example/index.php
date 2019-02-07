88d07b0126038253fab8f5d935a51c90522361bc
OnoprienkoArtem <o.artem.e@gmail.com>
2019-02-07 22:04:44 +0200

    practice ajax

---
+++ network(ajax, rest-api)/example/index.php
@@ -0,0 +1,14 @@
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>AJAX</title>
+        <script type="text/javascript" src="ajax.js"></script>
+    </head>
+    <body>
+        введите свое имя:
+        <input type="text" id="Name" name="" value="">
+        <input type="button" name="mySubmit" value="submit" onClick="ajax()">
+        <div id="OurMessage"></div>
+    </body>
+</html>
