f94d537740cfa9fa70193f50936c5fd6a39e14bd
OnoprienkoArtem <o.artem.e@gmail.com>
2019-03-10 23:00:06 +0200

    js practice

---
+++ patterns/patterns.js
@@ -0,0 +1,38 @@
+// Паттерны (шаблоны) проектирования - это  типовые подходы к решению задач возникающих в процессе проектирования 
+// вашего приложения. Паттерны предлагают определенную идею или схему как можно зешить ту или иную задачу.
+
+// Существует четыре типа шаблонов:
+
+// - порождающие (Creational) - описывают подходы и механизмы инициализации объектов;
+// Creational patterns:
+// -- Builder
+// -- Factory
+// -- Prototype
+// -- Singleton
+
+
+// - структурные (Structural) - определяют отношения между классами и объектами, позволяя им работать совместно;
+// Structural patterns:
+// -- Module
+// -- Adapter
+// -- Bridge
+// -- Composite
+// -- Decorator
+// -- Facade
+// -- Proxy
+
+
+// - поведенческие (Behavioral) - описывают различные взаимодействия между объектами и структурами;
+// -- Chain of Responsibility
+// -- Command
+// -- Iterator
+// -- Mediator
+// -- Memento
+// -- Observer
+// -- Strategy
+
+
+// - архитектурные (Architectural) - описывают общую архитектуру приложения в целом;
+// -- Model View Controller
+// -- Model View Presenter
+// -- Model View ViewModel
