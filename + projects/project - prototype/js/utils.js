9ed25756830a2db087e0e5e3dd0e6e6c23a8316c
OnoprienkoArtem <o.artem.e@gmail.com>
2019-03-29 23:47:50 +0200

    create file for utils

---
+++ + projects/project - prototype/js/utils.js
@@ -0,0 +1,16 @@
+const utils = {
+    inheritance: function (parent, child) {
+        const tempChild = child.prototype;
+        child.prototype = Object.create(parent.prototype);
+        child.prototype.constructor = child;
+
+        for (let key in tempChild) {
+            if (tempChild.hasOwnProperty(key)) {
+                child.prototype[key] = tempChild[key];
+            }
+        }
+    },
+
+
+ 
+};
