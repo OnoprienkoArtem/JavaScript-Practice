80460f111c1932e8e5e47c91e4e81fe9d1bcc107
OnoprienkoArtem <o.artem.e@gmail.com>
2019-04-16 23:09:15 +0300

    add base gallery prototype+

--- + projects/project - prototype/js/gallery.js
+++ + projects/project - prototype/js/gallery.js
@@ -6,3 +6,17 @@ const BaseGalleryModule = function () {
     // code
     // this.view = view;
 };
+
+BaseGalleryModule.prototype = {
+    init: function () {
+        this.hide()
+    },
+
+    show: function () {  
+        this.view.wrapper.classList.remove('hide');
+    },
+
+    hide: function () {
+        this.view.wrapper.classList.add('hide');
+    }
+};
