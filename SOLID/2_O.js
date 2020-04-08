d739297ec15d88e592ac290ff4fd539b3f98dc2f
OnoprienkoArtem <o.artem.e@gmail.com>
2020-04-08 22:33:44 +0300

    explore open close principle

---
+++ SOLID/2_O.js
@@ -0,0 +1,14 @@
+// Open close principle
+
+class Square {
+    constructor(size) {
+        this.size = size;
+    }
+}
+
+
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+}
