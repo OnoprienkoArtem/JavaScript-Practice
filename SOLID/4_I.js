3ea58f9204d6351e0591cfa7d3517b33d7ae8ca7
OnoprienkoArtem <o.artem.e@gmail.com>
2020-04-12 23:37:11 +0300

    example with interface segregation

--- SOLID/4_I.js
+++ SOLID/4_I.js
@@ -1,5 +1,48 @@
 // INTERFACE SEGREGATION PRINCIPLE
 
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+const walker = {
+    walk() {
+        console.log(`${this.name} can walk`)
+    }
+};
+
+const swimmer = {
+    swim() {
+        console.log(`${this.name} can swim`)
+    }
+};
+
+const flier = {
+    fly() {
+        console.log(`${this.name} can fly`)
+    }
+};
+
+class Dog extends Animal {}
+class Eagle extends Animal {}
+class Whale extends Animal {}
+
+Object.assign(Dog.prototype, walker, swimmer);
+Object.assign(Eagle.prototype, walker, flier);
+Object.assign(Whale.prototype, swimmer);
+
+const dog = new Dog('Max');
+dog.walk();
+dog.swim();
+
+const eagle = new Eagle('Eag');
+eagle.walk();
+eagle.fly();
+
+const whale = new Whale('Big');
+whale.swim();
+
 
 
 
